a2199832a49400e9765edac29a7fd6c6c5bb945d
Danielle Barker <caseybarker86@gmail.com>
2021-03-27 16:20:36 -0600

    scraper

---
+++ utilities/scraper_elements.py
@@ -0,0 +1,48 @@
+# Choose English
+CE = '/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/button'
+# Login Username
+LU = '/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/table[1]/tbody/tr/td[1]/input'
+# Login Password
+LP ='/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/input'
+# Connect Button
+CB = '/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/table[1]/tbody/tr/td[3]/button'
+# Command Text Box
+CT = '/html/body/div[4]/div[2]/div/div[2]/table/tbody/tr/td[2]/input'
+
+# Selected Game Button
+def VG(n):
+    return f'/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/div/div[3]/div/div[8]/div/div/table/tbody/tr[{n}]/td/table/tbody/tr/td/table/tbody/tr/td[1]/div'
+def SG(n): 
+    return f'/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/div/div[3]/div/div[8]/div/div/table/tbody/tr[{n}]/td/table/tbody/tr/td/table/tbody/tr/td[2]/span'
+# Player1
+# def P1(n):
+#     return f'/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/div/div[3]/div/div[8]/div/div/div/table[{n}]/tbody/tr/td[2]/button'
+# # Player2
+# def P2(n):
+#     return f'/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/div/div[3]/div/div[8]/div/div/div/table[{n}]/tbody/tr/td[4]/button'
+# Close History Button
+CL = '/html/body/div[5]/div/table/tbody/tr[2]/td[2]/div/div/table/tbody/tr/td[7]/button'
+# Player 1 Name/Score
+P1N = '/html/body/div[4]/div[2]/div/div[1]/table[2]/tbody/tr/td[1]/div'
+# Player 2 Name/Score
+P2N = '/html/body/div[4]/div[2]/div/div[1]/table[2]/tbody/tr/td[2]/div'
+# Player 1 Clock
+       
+P1C = '/html/body/div[4]/div[2]/div/div[1]/table[3]/tbody/tr/td/div'
+# Player 2 Clock
+P2C = '/html/body/div[4]/div[2]/div/div[1]/table[4]/tbody/tr/td/div'    
+# Start of Game Button
+ST = '/html/body/div[4]/div[2]/div/div[1]/table[15]/tbody/tr/td/table/tbody/tr/td[1]/div'
+# Tiles
+# def TI(n):
+#     return f'/html/body/div[4]/div[2]/div/div[1]/div[{464 + n}]'
+# Next Move Button
+
+NM = '/html/body/div[4]/div[2]/div/div[1]/table[19]/tbody/tr/td/table/tbody/tr/td[1]/div'
+
+# Chat Window
+CW = '/html/body/div[4]/div[2]/div/div[2]/div/div/div/div'
+
+                            
+
+                     
